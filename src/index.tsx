70666d624aefe64ed064349be5211443a1479bd3
Arye Eidelman <aryedov.e@gmail.com>
2022-02-28 00:04:01 -0500

    Setup react-router v6
    Holding at https://reactrouter.com/docs/en/v6/getting-started/tutorial#listing-the-invoices

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/tailwind.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Form from "./routes/Form";
+import Home from "./routes/Home";
+import NewForm from "./routes/NewForm";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} >
+        <Route path="" element={<Home />} />
+        <Route path="forms/new" element={<NewForm />} />
+        <Route path="forms/:slug" element={<Form />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/routes/NewForm.tsx
@@ -0,0 +1,7 @@
+export default function NewForm() {
+  return (
+    <div>
+      <h1>New Form</h1>
+    </div>
+  )
+}
